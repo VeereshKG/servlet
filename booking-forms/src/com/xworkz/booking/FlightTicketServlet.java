6f51ceefef5d1aa76892e56b5a41310d6e5f8c68
kgveeresh683@gmail.com <kgveeresh683@gmail.com>
2025-11-13 08:46:12 +0530

    cookie

--- booking-forms/src/com/xworkz/booking/FlightTicketServlet.java
+++ booking-forms/src/com/xworkz/booking/FlightTicketServlet.java
@@ -2,6 +2,7 @@ package com.xworkz.booking;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,18 +27,52 @@ public class FlightTicketServlet extends HttpServlet {
         String instructions = req.getParameter("instructions");
         String classType = req.getParameter("classType");
 
+
+        Cookie[]cookies = req.getCookies();
+        if (cookies == null || cookies.length<1){
+            throw new IllegalArgumentException("Cookie is not Created Go To Index page And Create Cookie......");
+        }else {
+            for (Cookie cookie : cookies)
+                System.out.println("The name of the Cookie Name is:"+cookie.getName()+"Value of the Cookie is:"+cookie.getValue());
+        }
+
+
+
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
 
-        writer.println("<h1 style='color: green;'> Flight Booked Successfully </h1>");
-        writer.println("<h3 style='color: blue;'>Passenger Details Are:</h3>");
-        writer.println("Passenger Name : " + passengerName + "<br>");
-        writer.println("Source : " + source + "<br>");
-        writer.println("Destination : " + destination + "<br>");
-        writer.println("Special Instructions : " + instructions + "<br>");
-        writer.println("Class Type : " + classType + "<br>");
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>Flight Booking Confirmation</title>");
+        writer.println("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'>");
+        writer.println("</head>");
+        writer.println("<body class='bg-light'>");
 
-        writer.println("<a href='FilghtTicket.html' target='blank' style:'color:red'>Click here To Another Flight Booking</a>");
+        writer.println("<div class='container mt-5'>");
+        writer.println("<div class='card shadow-lg p-4'>");
+
+        writer.println("<h1 class='text-success text-center mb-4'>Flight Booked Successfully</h1>");
+        writer.println("<h3 class='text-primary mb-3'>Passenger Details Are:</h3>");
+
+        writer.println("<ul class='list-group mb-4'>");
+        writer.println("<li class='list-group-item'><strong>Passenger Name:</strong> " + passengerName + "</li>");
+        writer.println("<li class='list-group-item'><strong>Source:</strong> " + source + "</li>");
+        writer.println("<li class='list-group-item'><strong>Destination:</strong> " + destination + "</li>");
+        writer.println("<li class='list-group-item'><strong>Special Instructions:</strong> " + instructions + "</li>");
+        writer.println("<li class='list-group-item'><strong>Class Type:</strong> " + classType + "</li>");
+        writer.println("</ul>");
+
+        writer.println("<div class='text-center'>");
+        writer.println("<a href='FlightTicket.html' target='_blank' class='btn btn-danger'>Book Another Flight</a>");
+        writer.println("</div>");
+
+        writer.println("</div>");
+        writer.println("</div>");
+
+        writer.println("</body>");
+        writer.println("</html>");
+
 
 
     }
--- booking-forms/src/com/xworkz/booking/FoodOrderServlet.java
+++ booking-forms/src/com/xworkz/booking/FoodOrderServlet.java
@@ -2,6 +2,7 @@ package com.xworkz.booking;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,18 +28,50 @@ public class FoodOrderServlet extends HttpServlet {
         String address = req.getParameter("address");
         String paymentMode = req.getParameter("paymentMode");
 
+        Cookie[]cookies = req.getCookies();
+        if (cookies == null || cookies.length<1){
+            throw new IllegalArgumentException("Cookie is not Created Go To Index page And Create Cookie......");
+        }else {
+            for (Cookie cookie : cookies)
+                System.out.println("The name of the Cookie Name is:"+cookie.getName()+"Value of the Cookie is:"+cookie.getValue());
+        }
+
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
 
-        writer.println("<h1 style='color: green;'> Order Received Successfully </h1>");
-        writer.println("<h3 style='color: blue;'> Order Details Are:</h3>");
-        writer.println("Restaurant Name : " + customerName + "<br>");
-        writer.println("Ordered Items : " + items + "<br>");
-        writer.println("Customer Name : " + name + "<br>");
-        writer.println("Phone Number : " + phoneNumber + "<br>");
-        writer.println("Address : " + address + "<br>");
-        writer.println("Payment Mode : " + paymentMode + "<br>");
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>Food Order Confirmation</title>");
+        writer.println("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'>");
+        writer.println("</head>");
+        writer.println("<body class='bg-light'>");
 
-        writer.println("<a href='FoodOrder.html' target='blank' style:'color:red'>Click here To Order Again</a>");
+        writer.println("<div class='container mt-5'>");
+        writer.println("<div class='card shadow-lg p-4'>");
+
+        writer.println("<h1 class='text-success text-center mb-4'>Order Received Successfully</h1>");
+        writer.println("<h3 class='text-primary mb-3'>Order Details Are:</h3>");
+
+        writer.println("<ul class='list-group mb-4'>");
+        writer.println("<li class='list-group-item'><strong>Restaurant Name:</strong> " + customerName + "</li>");
+        writer.println("<li class='list-group-item'><strong>Ordered Items:</strong> " + items + "</li>");
+        writer.println("<li class='list-group-item'><strong>Customer Name:</strong> " + name + "</li>");
+        writer.println("<li class='list-group-item'><strong>Phone Number:</strong> " + phoneNumber + "</li>");
+        writer.println("<li class='list-group-item'><strong>Address:</strong> " + address + "</li>");
+        writer.println("<li class='list-group-item'><strong>Payment Mode:</strong> " + paymentMode + "</li>");
+        writer.println("</ul>");
+
+        writer.println("<div class='text-center'>");
+        writer.println("<a href='FoodOrder.html' target='_blank' class='btn btn-danger'>Click Here to Order Again</a>");
+        writer.println("</div>");
+
+        writer.println("</div>");
+        writer.println("</div>");
+
+        writer.println("</body>");
+        writer.println("</html>");
+
+
     }
 }
